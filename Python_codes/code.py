6b85091f7183396bb143cea27899c847910f7b55
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-11-08 18:33:15 -0800

    using Tuple wit For

--- Python_codes/code.py
+++ Python_codes/code.py
@@ -1,7 +1,4 @@
-my_list = [1,2,3,4,5]
-for num in my_list:
-    print(num * 2)
-    print(my_list)  
-# for num in my_list:
-#     print(num * 2)
-# print(my_list)
+my_tuple = (1,2,3,4,"Hello")
+for a in my_tuple:
+    print(a)
+    
