7971b0b9b4a99716627c73c9d53ce43a05f76d8f
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-11-08 17:46:45 -0800

    using or condition

--- Python_codes/code.py
+++ Python_codes/code.py
@@ -9,11 +9,13 @@ if user_choice in choices:
     #r›s - p›r - s›p
     if user_choice == ai_choice:
         print( 'Tie')
-    elif user_choice == 'r' and ai_choice == 's' :
-        print( 'You Won!')
-    elif user_choice == 'p' and ai_choice == 'r':
-        print( 'You Won!')
-    elif user_choice == 's' and ai_choice == 'p':
+    # elif user_choice == 'r' and ai_choice == 's' :
+    #     print( 'You Won!')
+    # elif user_choice == 'p' and ai_choice == 'r':
+    #     print( 'You Won!')
+    # elif user_choice == 's' and ai_choice == 'p':
+    #     print( 'You Won!')
+    elif (user_choice == 'r' and ai_choice == 's') or (user_choice == 'p' and ai_choice == 'r') or (user_choice == 's' and ai_choice == 'p'):
         print( 'You Won!')
     else:
         print( 'You Lost!')
